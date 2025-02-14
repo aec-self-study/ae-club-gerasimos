d980b68ba2faf9c51d68b156d07e79a95fe4ea47
makismrk <makismrk2005@gmail.com>
2025-02-14 12:14:10 +0000

    Add customers query

---
+++ customers.sql
@@ -0,0 +1,9 @@
+select 
+customers.id as customer_id,
+customers.name,
+customers.email,
+min(created_at) over (partition by orders.id) as first_order_at,
+count(*) over (partition by customers.id) as number_of_orders
+from `analytics-engineers-club.coffee_shop.customers` as customers
+left join `analytics-engineers-club.coffee_shop.orders` as orders
+on ( customers.id = orders.customer_id )
